8d3c37b15bbc30368f7f4edf30ad523b7fd0b7b2
Shu Uesugi <chibicode@gmail.com>
2012-09-09 06:36:55 +0900

    initial commit with rspec and cucumber

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HitokijiCom::Application.config.secret_token = '8af66139235e1dd3981b09dbf7ec9d7ffb3b3b3985c0ee8021ed7afc7366ae145e89ffeb036e6da62b2ad61d1cda5c904bf66877f46dbb2c94c5a461fe43d065'
